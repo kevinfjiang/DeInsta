c90242de473d5a17b842eef920a72f679a061b69
kevinfjiang <kfj2112@columbia.edu>
2021-12-04 12:22:59 -0500

    more follower following support and added lots testing support

--- backend/contracts/DIAccount.sol
+++ backend/contracts/DIAccount.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.8.0;
 pragma abicoder v2;
 // SPDX-License-Identifier: MIT
 
+//Add donate money feature
 contract DIAccount {
     string public Name;
     string public Bio;
@@ -26,9 +27,15 @@ contract DIAccount {
         Name = Name_;
         CountPosts = 0;
         owner = msg.sender;
-        Followers.push(msg.sender);
-        Following.push(msg.sender);
+        Followers.push(address(this));
+        Following.push(address(this));
     }
+
+    receive() external payable{
+        require(msg.value > 0, "Sum must be greater than 0");
+        payable(owner).transfer(msg.value);
+    }
+
     
     modifier isAdmin(){
         require(msg.sender == owner);
@@ -36,10 +43,15 @@ contract DIAccount {
     }
 
     modifier isDIAccount(){
-        require(DIAccount(msg.sender).Followers(0)==msg.sender);
+        require(DIAccount(payable(msg.sender)).Followers(0)==msg.sender);
         _;
     }
     
+    modifier isValidVote(int8 currVote, int8 vote){
+        require(currVote != vote, "You must change your vote!");
+        _;
+
+    }
 
     function SetBio(string memory Bio_) external isAdmin {
        Bio=Bio_;
@@ -48,6 +60,7 @@ contract DIAccount {
     function DeletePost(uint PostID) external isAdmin{
         delete Posts[PostID];
     }
+
     
     function DeleteAccount() external payable isAdmin {
         selfdestruct(payable(owner));
@@ -93,6 +106,7 @@ contract DIAccount {
     // create new post
     function CreatePost(string memory PostURL_, string memory Caption_) external isAdmin{
         require(bytes(Caption_).length < 50, "Post exceeds character limit"); 
+        Posts.push(); // Fuck dumb shit
         Post storage newPost = Posts[CountPosts];
         
         newPost.timestamp = block.timestamp;
@@ -107,13 +121,14 @@ contract DIAccount {
     }
 
     function Vote(address targ, int8 vote, uint PostNumber) external {
-        vote = vote>0? int8(1) : (vote<0? -1:int8(0));
-        require(Posts[PostNumber].voters[msg.sender] != vote, "You've already voted");
-        DIAccount(targ).VoteOwner(vote, PostNumber);
+        DIAccount(payable(targ)).VoteOwner(vote>0? int8(1) : (vote<0? -1:int8(0)), PostNumber);
     }
 
     // Can't wait to get generics/templates or interfaces
-    function VoteOwner(int8 vote, uint PostNumber) external isDIAccount{
+    function VoteOwner(int8 vote, uint PostNumber) 
+      external 
+      isDIAccount
+      isValidVote(Posts[PostNumber].voters[msg.sender], vote){
         Posts[PostNumber].Karma-=Posts[PostNumber].voters[msg.sender];
         Karma-=Posts[PostNumber].voters[msg.sender];
         Posts[PostNumber].Karma+=vote;
@@ -124,10 +139,11 @@ contract DIAccount {
 
     function AddComment(address targ, uint PostNumber, string memory CommentString) external {
         require(bytes(CommentString).length < 50, "Post exceeds character limit");
-        DIAccount(targ).AddCommentOwner(PostNumber, CommentString);
+        DIAccount(payable(targ)).AddCommentOwner(PostNumber, CommentString);
     }
 
     function AddCommentOwner(uint PostNumber, string memory CommentString) external isDIAccount{
+        Posts[PostNumber].Comments.push();
         Comment storage newComment = Posts[PostNumber].Comments[Posts[PostNumber].CountComments];
 
         newComment.timestamp = block.timestamp;
@@ -140,28 +156,30 @@ contract DIAccount {
     }
 
     function CommentVote(address targ, int8 vote, uint PostNumber, uint CommentNumber) external {
-        vote = vote>0? int8(1): (vote<0? -1: int8(0));
-        require(Posts[PostNumber].CommentVoters[CommentNumber][msg.sender] != vote, "You've already voted");
-        DIAccount(targ).CommentVoteOwner(vote, PostNumber, CommentNumber);
+        DIAccount(payable(targ)).CommentVoteOwner(vote>0? int8(1) : (vote<0? -1:int8(0)), PostNumber, CommentNumber);
     }
 
-    function CommentVoteOwner(int8 vote, uint PostNumber, uint CommentNumber) external {
+    function CommentVoteOwner(int8 vote, uint PostNumber, uint CommentNumber) 
+      external 
+      isDIAccount
+      isValidVote(Posts[PostNumber].CommentVoters[CommentNumber][msg.sender], vote){
         Posts[PostNumber].Comments[CommentNumber].Karma-=Posts[PostNumber].CommentVoters[CommentNumber][msg.sender];
         Posts[PostNumber].Comments[CommentNumber].Karma+=vote;
         Posts[PostNumber].CommentVoters[CommentNumber][msg.sender]=vote;
     }
 
     function Follow(address targ) external {
-        require(followersIndex[targ]==0 && DIAccount(targ).NewFollowing());
+        require(followersIndex[targ]==0 && DIAccount(payable(targ)).NewFollowing());
         Followers.push(targ);
         followersIndex[targ] = Followers.length-1;
     }
 
     function UnFollow(address targ) external {
-        require(followersIndex[targ]!=0 && DIAccount(targ).UnNewFollowing());
+        require(followersIndex[targ]!=0 && DIAccount(payable(targ)).UnNewFollowing());
         Followers[followersIndex[targ]] = Followers[Followers.length - 1];
+        followersIndex[Followers[Followers.length - 1]]=followersIndex[targ]; 
+        followersIndex[targ] = 0;
         Followers.pop();
-        followingIndex[targ] = 0;
     }
 
     function NewFollowing() external isDIAccount returns(bool added){
@@ -173,8 +191,10 @@ contract DIAccount {
 
     function UnNewFollowing() external isDIAccount returns(bool removed){
         require(followingIndex[msg.sender]!=0);
-        Following.push(msg.sender);
-        followingIndex[msg.sender] = Followers.length-1;
+        Following[followingIndex[msg.sender]] = Following[Followers.length - 1];
+        followingIndex[Following[Following.length - 1]]=followingIndex[msg.sender]; 
+        followingIndex[msg.sender] = 0;
+        Following.pop();
         return true;
     }
 
